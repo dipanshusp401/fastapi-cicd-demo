d3848431c85c4feb66e4c1ad226f4d424aef974f
dipanshusp401 <dipanshusp401@gmail.com>
2026-06-15 13:04:45 +0530

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,8 +6,8 @@ client = TestClient(app)
 def test_home():
     response = client.get("/")
     assert response.status_code == 200
-    # assert response.json() == {"message": "Hello from Docker!"}
-    assert response.json() == {"message": "Wrong message!"}
+    assert response.json() == {"message": "Hello from Docker!"}
+    # assert response.json() == {"message": "Wrong message!"}
 def test_health():
     response = client.get("/health")
     assert response.status_code == 200
